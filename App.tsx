4644ca4085a9325cb789a1b732fc8e9081be3d98
Luiz Almeida <luizhrqas@gmail.com>
2021-12-27 12:29:46 -0300

    initialize home

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  NativeBaseProvider,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Image,
+} from "native-base";
+import Illustration from "./assets/undraw_fast_car_p-4-cu.svg";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Box flex="1" alignItems="center" justifyContent="center" px={10}>
+        <StatusBar style="auto" />
+        <Illustration width="100%" height="30%" />
+        <Heading>CNH Simulado Prova</Heading>
+        <Text fontSize="sm" mt={4}>
+          Preparado para testar seus conhecimentos?
+        </Text>
+        <Button width="100%" mt={5} size="lg">
+          COMEÇAR
+        </Button>
+        <Button width="100%" mt={4} variant="subtle">
+          RANKING
+        </Button>
+        <Button width="100%" mt={4} variant="subtle">
+          AVALIAR APP
+        </Button>
+      </Box>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
